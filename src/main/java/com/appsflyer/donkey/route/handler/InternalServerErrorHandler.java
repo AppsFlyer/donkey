d58187ec129f241441b4f58f6c06dcde0db351f2
yaron.elyashiv <yaron.elyashiv@appsflyer.com>
2020-08-13 17:26:42 +0300

    Added support for string response body

--- src/main/java/com/appsflyer/donkey/route/handler/InternalServerErrorHandler.java
+++ src/main/java/com/appsflyer/donkey/route/handler/InternalServerErrorHandler.java
@@ -3,10 +3,12 @@ package com.appsflyer.donkey.route.handler;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.ext.web.RoutingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class InternalServerErrorHandler extends ResponseBuilder implements Handler<RoutingContext>
 {
-  
+  private static final Logger logger = LoggerFactory.getLogger(InternalServerErrorHandler.class.getName());
   public InternalServerErrorHandler(Vertx vertx)
   {
     super(vertx);
@@ -15,6 +17,8 @@ public class InternalServerErrorHandler extends ResponseBuilder implements Handl
   @Override
   public void handle(RoutingContext ctx)
   {
+    Throwable ex = ctx.failure();
+    logger.error("Unhandled exception:", ex);
     addDefaultHeaders(ctx.response());
     ctx.response().setStatusCode(500).end();
   }
