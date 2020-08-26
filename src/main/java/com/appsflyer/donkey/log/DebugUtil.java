d268c452b4a570a1fc478da18145daf434c59718
yaron.elyashiv <yaron.elyashiv@appsflyer.com>
2020-08-26 10:00:40 +0300

    Gracefully fail when debug mode is on and Logback is unavailable

--- src/main/java/com/appsflyer/donkey/log/DebugUtil.java
+++ src/main/java/com/appsflyer/donkey/log/DebugUtil.java
@@ -4,9 +4,15 @@ import org.slf4j.Logger;
 
 public final class DebugUtil {
   
+  private static final String LOGBACK_LOGGER_FQCN = "ch.qos.logback.classic.Logger";
+  
   private DebugUtil() {}
   
   public static void enableDebugLogging() {
+    if (!isLogbackAvailable()) {
+      return;
+    }
+    
     var root = LogbackLoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
     var vertx = LogbackLoggerFactory.getLogger("io.vertx");
     var netty = LogbackLoggerFactory.getLogger("io.netty");
@@ -16,5 +22,18 @@ public final class DebugUtil {
     vertx.setLevel("TRACE");
     netty.setLevel("DEBUG");
     appsflyer.setLevel("DEBUG");
+  }
+  
+  private static boolean isLogbackAvailable() {
+    ClassLoader loader = Thread.currentThread().getContextClassLoader();
+    try {
+      loader.loadClass(LOGBACK_LOGGER_FQCN);
+      return true;
+    } catch (ClassNotFoundException ex) {
+      //noinspection UseOfSystemOutOrSystemErr // We couldn't find the logger implementation so we assume there is none, and therefore print to stderr.
+      System.err.printf("%s - Warning! Did not find %s on classpath. Debug logging will be disabled.%n",
+                        DebugUtil.class.getCanonicalName(), LOGBACK_LOGGER_FQCN);
+      return false;
+    }
   }
 }
