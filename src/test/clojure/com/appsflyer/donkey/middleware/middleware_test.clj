9924bd04f36773811f5734f0fce93ed18ca097af
yaron.elyashiv <yaron.elyashiv@appsflyer.com>
2020-08-27 19:20:25 +0300

    Added non-blocking middleware test

--- src/test/clojure/com/appsflyer/donkey/middleware/middleware_test.clj
+++ src/test/clojure/com/appsflyer/donkey/middleware/middleware_test.clj
@@ -6,24 +6,28 @@
   (:import (io.vertx.ext.web.client HttpRequest)
            (clojure.lang Symbol Keyword)))
 
+(defn- test-case [uri]
+  (let [response-promise (promise)
+        query-string "foo=bar&_count=6&:valid=true&:-empty=false&1=2&_"]
+    (-> util/client
+        ^HttpRequest (.get (str uri "?" query-string))
+        (.send (util/create-client-handler response-promise)))
+
+    (let [res (util/parse-response-body-when-resolved response-promise)
+          current-ns (str (the-ns *ns*))]
+
+      (is (= query-string (:query-string res)))
+      (is (= "bar" (-> res :query-params :foo)))
+      (is (= "6" (-> res :query-params :_count)))
+      (is (= "true" (get-in res [:query-params (Keyword/intern (Symbol/create current-ns "valid"))])))
+      (is (= "false" (get-in res [:query-params (Keyword/intern (Symbol/create current-ns "-empty"))])))
+      (is (= "2" (-> res :query-params :1)))
+      (is (contains? (:query-params res) :_)))))
+
 (deftest test-keywordize-query-params
   (util/init
     (fn []
-      (let [response-promise (promise)
-            query-string "foo=bar&_count=6&:valid=true&:-empty=false&1=2&_"]
-        (-> util/client
-            ^HttpRequest (.get (str "/echo?" query-string))
-            (.send (util/create-client-handler response-promise)))
-
-        (let [res (util/parse-response-body-when-resolved response-promise)
-              current-ns (str (the-ns *ns*))]
-
-          (is (= query-string (:query-string res)))
-          (is (= "bar" (-> res :query-params :foo)))
-          (is (= "6" (-> res :query-params :_count)))
-          (is (= "true" (get-in res [:query-params (Keyword/intern (Symbol/create current-ns "valid"))])))
-          (is (= "false" (get-in res [:query-params (Keyword/intern (Symbol/create current-ns "-empty"))])))
-          (is (= "2" (-> res :query-params :1)))
-          (is (contains? (:query-params res) :_)))))
-    [routes/echo-route]
+      (test-case "/echo")
+      (test-case "/echo/non-blocking"))
+    [routes/echo-route routes/echo-route-non-blocking]
     [keywordize-query-params]))
