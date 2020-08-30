7a039842853ffea56313a974a7ddc043384ca795
yaron.elyashiv <yaron.elyashiv@appsflyer.com>
2020-08-30 11:48:00 +0300

    Fix race conditions when starting /stopping the server in tests

--- src/test/java/com/appsflyer/donkey/route/handler/DateHeaderGeneratorTest.java
+++ src/test/java/com/appsflyer/donkey/route/handler/DateHeaderGeneratorTest.java
@@ -1,11 +1,14 @@
 package com.appsflyer.donkey.route.handler;
 
 import com.appsflyer.donkey.server.Server;
+import com.appsflyer.donkey.server.exception.ServerInitializationException;
+import com.appsflyer.donkey.server.exception.ServerShutdownException;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.junit5.Checkpoint;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,15 +24,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class DateHeaderGeneratorTest {
   
   private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.RFC_1123_DATE_TIME;
+  private Server server;
+  
+  @AfterEach
+  void tearDown() throws ServerShutdownException {
+    server.shutdownSync();
+    server = null;
+  }
   
   @Test
-  void testDateHeaderNotIncludedByDefault(Vertx vertx, VertxTestContext testContext) {
-    var server = Server.create(getDefaultConfigBuilder().build());
-    server.start();
+  void testDateHeaderNotIncludedByDefault(Vertx vertx, VertxTestContext testContext) throws
+                                                                                     ServerInitializationException {
+    server = Server.create(getDefaultConfigBuilder().build());
+    server.startSync();
     
     doGet(vertx, "/")
         .onComplete(testContext.succeeding(response -> testContext.verify(() -> {
-          server.shutdown();
           assert200(response);
           assertNull(response.getHeader("Date"));
           testContext.completeNow();
@@ -37,22 +47,17 @@ class DateHeaderGeneratorTest {
   }
   
   @Test
-  void testAddingDateHeader(Vertx vertx, VertxTestContext testContext) {
-    var server = Server.create(getDefaultConfigBuilder()
-                                   .addDateHeader(true)
-                                   .build());
-    server.start();
-  
+  void testAddingDateHeader(Vertx vertx, VertxTestContext testContext) throws
+                                                                       ServerInitializationException {
+    server = Server.create(getDefaultConfigBuilder()
+                               .addDateHeader(true)
+                               .build());
+    server.startSync();
+    
     Checkpoint responsesReceived = testContext.checkpoint(2);
-  
+    
     AtomicReference<String> lastDate = new AtomicReference<>("");
     Handler<Long> testCase = v -> doGet(vertx, "/")
-        .onComplete(ignore -> {
-          //Second time we can shutdown the server
-          if (!lastDate.get().isBlank()) {
-            server.shutdown();
-          }
-        })
         .onComplete(testContext.succeeding(response -> testContext.verify(() -> {
           assert200(response);
           // Assert that the Date header is a valid date, and that it changed
@@ -62,10 +67,10 @@ class DateHeaderGeneratorTest {
           assertValidDate(date);
           responsesReceived.flag();
         })));
-  
+    
     //Call one time now
     testCase.handle(0L);
-  
+    
     //Call again in one second
     vertx.setTimer(1000, testCase);
   }
--- src/test/java/com/appsflyer/donkey/route/handler/ServerHeaderHandlerTest.java
+++ src/test/java/com/appsflyer/donkey/route/handler/ServerHeaderHandlerTest.java
@@ -1,9 +1,11 @@
 package com.appsflyer.donkey.route.handler;
 
 import com.appsflyer.donkey.server.Server;
+import com.appsflyer.donkey.server.exception.ServerShutdownException;
 import io.vertx.core.Vertx;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,32 +18,37 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 @ExtendWith(VertxExtension.class)
 class ServerHeaderHandlerTest {
   
+  private Server server;
+  
+  @AfterEach
+  void tearDown() throws ServerShutdownException {
+    server.shutdownSync();
+    server = null;
+  }
+  
   @Test
   void testDateHeaderNotIncludedByDefault(Vertx vertx, VertxTestContext testContext) {
-    var server = Server.create(getDefaultConfigBuilder().build());
-    server.start();
-    
-    doGet(vertx, "/")
-        .onComplete(testContext.succeeding(response -> testContext.verify(() -> {
-          server.shutdown();
-          assert200(response);
-          assertNull(response.getHeader("Server"));
-          testContext.completeNow();
-        })));
+    server = Server.create(getDefaultConfigBuilder().build());
+    server.start()
+          .onSuccess(
+              v -> doGet(vertx, "/")
+                  .onComplete(testContext.succeeding(response -> testContext.verify(() -> {
+                    assert200(response);
+                    assertNull(response.getHeader("Server"));
+                    testContext.completeNow();
+                  }))));
   }
   
   @Test
   void testAddingDateHeader(Vertx vertx, VertxTestContext testContext) {
-    var server = Server.create(getDefaultConfigBuilder().addServerHeader(true).build());
-    server.start();
-    
-    doGet(vertx, "/")
-        .onComplete(testContext.succeeding(response -> testContext.verify(() -> {
-          server.shutdown();
-          assert200(response);
-          assertEquals(ServerHeaderHandler.SERVER_NAME, response.getHeader("Server"));
-          testContext.completeNow();
-        })));
-    
+    server = Server.create(getDefaultConfigBuilder().addServerHeader(true).build());
+    server.start()
+          .onSuccess(
+              v -> doGet(vertx, "/")
+                  .onComplete(testContext.succeeding(response -> testContext.verify(() -> {
+                    assert200(response);
+                    assertEquals(ServerHeaderHandler.SERVER_NAME, response.getHeader("Server"));
+                    testContext.completeNow();
+                  }))));
   }
 }
