58ceaddfd4235a043ea618f1a24a4f86d91ed1f7
yaron.elyashiv <yaron.elyashiv@appsflyer.com>
2020-08-30 14:05:40 +0300

    metrics documentation

--- src/main/clojure/com/appsflyer/donkey/metrics.clj
+++ src/main/clojure/com/appsflyer/donkey/metrics.clj
@@ -1,22 +1,18 @@
 (ns com.appsflyer.donkey.metrics
-  (:import (io.vertx.ext.dropwizard DropwizardMetricsOptions Match MatchType)
+  (:import (io.vertx.ext.dropwizard DropwizardMetricsOptions)
            (io.vertx.core.metrics MetricsOptions)))
 
 (defn ^MetricsOptions get-metrics-options
-  ""
+  "Creates and returns a MetricsOptions object from the opts map.
+  The map may contain a `:metrics-prefix` string that will be prepended to each metric,
+  and a Dropwizard MetricRegistry instance under `:metric-registry`"
   [opts]
   (let [metrics-options (doto (DropwizardMetricsOptions.)
                           (.setEnabled true)
                           (.setJmxEnabled true)
-                          (.setBaseName (:metrics-prefix opts "donkey"))
-                          (.addMonitoredHttpServerUri
-                            (doto (Match.)
-                              (.setType MatchType/REGEX)
-                              (.setValue "/.*")
-                              (.setAlias "all"))))]
+                          (.setBaseName (:metrics-prefix opts "donkey")))]
 
-    (if (:metrics-registry opts)
-      (.setMetricRegistry metrics-options (:metrics-registry opts))
+    (if (:metric-registry opts)
+      (.setMetricRegistry metrics-options (:metric-registry opts))
       (.setRegistryName metrics-options "donkey-registry"))
-
     metrics-options))
