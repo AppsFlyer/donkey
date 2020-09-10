5c379ce86c26e8e23da32bb3d1afbcd4e0e83a7c
yaron.elyashiv <yaron.elyashiv@appsflyer.com>
2020-09-10 20:55:31 +0300

    Refactoring
    - Returning a FutureResult instead of promise from server start / stop
    - Extracted protocols and types to separate namespaces.

---
+++ src/main/clojure/com/appsflyer/donkey/request.clj
@@ -0,0 +1,53 @@
+(ns com.appsflyer.donkey.request
+  (:require [com.appsflyer.donkey.future])
+  (:import (clojure.lang IPersistentMap)
+           (io.vertx.ext.web.client HttpRequest)
+           (com.appsflyer.donkey.future FutureResult)
+           (com.appsflyer.donkey.client.ring RingClient)))
+
+(defprotocol IRequest
+  (submit [this] [this body]
+    "Submit an asynchronous request with an optional body. The body should be a
+    string or a byte[].
+    Returns a FutureResult that will be notified if the request succeeds or
+    fails.")
+  (submit-form [this body]
+    "Submit an asynchronous request as `application/x-www-form-urlencoded`. A
+    content-type header will be added to the request. If a `multipart/form-data`
+    content-type header already exists it will be used instead.
+    `body` is a map where all keys and values should be of type string.
+    The body will be urlencoded when it's submitted.
+    Returns a FutureResult that will be notified if the request succeeds or
+    fails.")
+  (submit-multipart-form [this body]
+    "Submit an asynchronous request as `multipart/form-data`. A content-type
+    header will be added to the request. You may use this function to send
+    attributes and upload files.
+    `body` is a map where all keys should be of type string. The values can be
+    either string for simple attributes, or a map of file options when uploading
+    a file.
+
+    The file options map should include the following values:
+    - filename: The name of the file, for example - image.png
+    - pathname: The absolute path of the file.
+      For example: /var/www/html/public/images/image.png
+    - media-type: The MimeType of the file. For example - image/png
+    - upload-as: Upload the file as `binary` or `text`. Default is `binary`
+
+    Returns a FutureResult that will be notified if the request succeeds or
+    fails."))
+
+(deftype AsyncRequest [^RingClient client ^HttpRequest req]
+  IRequest
+  (submit [_this]
+    (FutureResult.
+      (.send ^RingClient client ^HttpRequest req)))
+  (submit [_this body]
+    (FutureResult.
+      (.send ^RingClient client ^HttpRequest req body)))
+  (submit-form [_this body]
+    (FutureResult.
+      (.sendForm ^RingClient client ^HttpRequest req ^IPersistentMap body)))
+  (submit-multipart-form [_this body]
+    (FutureResult.
+      (.sendMultiPartForm ^RingClient client ^HttpRequest req ^IPersistentMap body))))
--- src/main/clojure/com/appsflyer/donkey/server.clj
+++ src/main/clojure/com/appsflyer/donkey/server.clj
@@ -1,12 +1,13 @@
 (ns com.appsflyer.donkey.server
-  (:require [com.appsflyer.donkey.route :refer [get-router-definition]])
-  (:import (io.vertx.core AsyncResult Handler)
-           (io.vertx.core.http HttpServerOptions)
+  (:require [com.appsflyer.donkey.route :refer [get-router-definition]]
+            [com.appsflyer.donkey.future])
+  (:import (io.vertx.core.http HttpServerOptions)
+           (io.vertx.core.impl.cpu CpuCoreSensor)
            (com.appsflyer.donkey.server Server ServerConfig)
            (com.appsflyer.donkey.server.exception ServerInitializationException ServerShutdownException)
            (com.appsflyer.donkey.route.ring RingRouteCreatorSupplier)
            (com.appsflyer.donkey.util DebugUtil)
-           (io.vertx.core.impl.cpu CpuCoreSensor)))
+           (com.appsflyer.donkey.future FutureResult)))
 
 (defn- ^HttpServerOptions get-server-options
   "Creates and returns an HttpServerOptions object from the opts map.
@@ -42,70 +43,37 @@
       (DebugUtil/enable))
     config))
 
-(deftype EventHandler [impl]
-  Handler
-  (handle [_this event]
-    (impl event)))
-
 (defprotocol HttpServer
   (start [this]
     "Start the server asynchronously.
-    Returns a promise that will be resolved with an
-    ExceptionInfo if the operation failed.
-    Otherwise resolved with nil.")
+    Returns a FutureResult that will be resolved with an ExceptionInfo if the
+    operation failed. Otherwise resolved with nil.")
   (start-sync [this]
     "Start the server synchronously.
     Blocks the calling thread until server initialization completes.
     Throws an ExceptionInfo if the operation failed.")
   (stop [this]
     "Stop the server asynchronously.
-    Returns a promise that will be resolved with an
-    ExceptionInfo if the operation failed.
-    Otherwise resolved with nil.")
+    Returns a FutureResult that will be resolved with an ExceptionInfo if the
+    operation failed. Otherwise resolved with nil.")
   (stop-sync [this]
     "Stop the server synchronously.
     Blocks the calling thread until all server resources are terminated.
-    Throws an ExceptionInfo if the operation failed.")
-  (await-termination [this]
-    "Blocks the calling thread until a shutdown signal is received
-    by the JVM. Useful in order to prevent exiting the '-main' function.
-    Throws an InterruptedException."))
+    Throws an ExceptionInfo if the operation failed."))
 
 (deftype DonkeyServer [^Server impl]
   HttpServer
   (start [_this]
-    (let [res (promise)]
-      (.onComplete
-        (.start ^Server impl)
-        (->EventHandler
-          (fn [^AsyncResult async-res]
-            (if (.failed async-res)
-              (deliver res (ex-info "Server initialization failed" {} (.cause async-res)))
-              (deliver res nil)))))
-      res))
-
+    (FutureResult. (.start ^Server impl)))
+  (stop [_this]
+    (FutureResult. (.shutdown ^Server impl)))
   (start-sync [_this]
     (try
       (.startSync impl)
       (catch ServerInitializationException ex
         (throw (ex-info (.getMessage ex) {} ex)))))
-
-  (stop [_this]
-    (let [res (promise)]
-      (.onComplete
-        (.shutdown ^Server impl)
-        (->EventHandler
-          (fn [^AsyncResult async-res]
-            (if (.failed async-res)
-              (deliver res (ex-info "Server shutdown failed" {} (.cause async-res)))
-              (deliver res nil)))))
-      res))
-
   (stop-sync [_this]
     (try
       (.shutdownSync impl)
       (catch ServerShutdownException ex
-        (throw (ex-info (.getMessage ex) ex)))))
-
-  (await-termination [_this]
-    (.awaitTermination ^Server impl)))
+        (throw (ex-info (.getMessage ex) ex))))))
