b5d6684861f0dd4d3037c1024d47721dde82d189
yaron.elyashiv <yaron.elyashiv@appsflyer.com>
2020-09-30 09:58:51 +0300

    Updated project urls

--- project.clj
+++ project.clj
@@ -1,11 +1,27 @@
+;
+; Copyright 2020 AppsFlyer
+;
+; Licensed under the Apache License, Version 2.0 (the "License")
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+;     http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+;
+
 (def ^:private vertx-version "3.9.2")
 (def ^:private junit-version "5.6.2")
 
 (defproject donkey "0.1.0-SNAPSHOT"
   :description "Clojure Web Server and Client"
-  :url "https://github.com/yaronel/donkey"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :url "https://github.com/AppsFlyer/donkey"
+  :license {:name "APACHE LICENSE, VERSION 2.0"
+            :url  "https://www.apache.org/licenses/LICENSE-2.0"}
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure" "src/test/java"]
   :java-source-paths ["src/main/java"]
